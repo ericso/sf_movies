5a364b4dcf50d26591e576236f3e368eca263958
Eric So <me@eric.so>
2015-02-28 17:18:37 -0800

    locations view takes title search parameter

--- locations/tests.py
+++ locations/tests.py
@@ -12,7 +12,6 @@ class ApiTest(TestCase):
   """
 
   def test_location_route_returns_200(self):
-
     response = self.client.get(
       '/locations/',
       HTTP_X_REQUESTED_WITH='XMLHttpRequest'
@@ -20,12 +19,47 @@ class ApiTest(TestCase):
     self.assertEqual(response.status_code, 200)
 
   def test_location_route_returns_json(self):
-
     response = self.client.get(
       '/locations/',
       HTTP_X_REQUESTED_WITH='XMLHttpRequest'
     )
-    data = response.content.decode()
 
-    self.assertEqual(json.loads(data), {'key': 'value'})
+    data = json.loads(response.content.decode())
 
+    single_film = {
+      "actor_1": "Siddarth",
+      "actor_2": "Nithya Menon",
+      "actor_3": "Priya Anand",
+      "director": "Jayendra",
+      "locations": "Epic Roasthouse (399 Embarcadero)",
+      "production_company": "SPI Cinemas",
+      "release_year": "2011",
+      "title": "180",
+      "writer": "Umarji Anuradha, Jayendra, Aarthi Sriram, & Suba "
+    }
+
+    self.assertIn(single_film, data['films'])
+
+  def test_location_route_takes_search_parameter(self):
+    response = self.client.get(
+      '/locations/',
+      {'search_text': '180'},
+      HTTP_X_REQUESTED_WITH='XMLHttpRequest'
+    )
+
+    data = json.loads(response.content.decode())
+
+    single_film = {
+      "actor_1": "Sarah Jones",
+      "actor_2": "Elizabeth Sarnoff",
+      "actor_3": "Bryan Wynbrandt",
+      "director": "J.J. Abrams",
+      "distributor": "Warner Bros. Television",
+      "locations": "Taylor St. from Broadway to Filbert",
+      "production_company": "Bonanza Productions Inc.",
+      "release_year": "2012",
+      "title": "Alcatraz",
+      "writer": "Steven Lilien"
+    }
+
+    self.assertNotIn(single_film, data['films'])
--- locations/views.py
+++ locations/views.py
@@ -2,14 +2,32 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 import json
+import requests
 
+
+API_ENDPOINT = 'https://data.sfgov.org/resource/yitu-d5am.json'
+API_KEY = 'iXBPg47nIqMKX8hBFqot8fTtH'
 
 def locations(request):
+  """Returns JSON response containing full query to SF Film API
+  """
 
-  if request.is_ajax():
-    print("request is ajax")
-  else:
-    print("request is NOT ajax")
+  search_text = request.GET.get('search_text', None)
 
-  data = {'key': 'value'}
-  return JsonResponse(data)
+  # Make a request to the SF Films API
+  params = {}
+  if search_text is not None:
+    params['title'] = search_text
+
+  headers = {'X-APP-TOKEN': API_KEY}
+  resp = requests.get(
+    API_ENDPOINT,
+    params=params,
+    headers=headers
+  )
+
+  films = resp.json()
+
+  return_data = {'films': films}
+
+  return JsonResponse(return_data)
