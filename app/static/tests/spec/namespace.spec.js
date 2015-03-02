bac1d86da65df770529f01d5889ba8ec9faf446c
Eric So <me@eric.so>
2015-03-02 10:34:22 -0800

    Add namespace JS app

---
+++ app/static/tests/spec/namespace.spec.js
@@ -0,0 +1,20 @@
+
+describe('Namespace', function() {
+
+  it("provides the 'App' object", function() {
+    expect(App).to.be.an('object');
+    expect(App).to.include.keys(
+      'Config',
+      'Models',
+      'Collections',
+      'Routers',
+      'Views',
+      'Templates'
+    );
+  });
+
+  it("provides the 'app' object", function() {
+    expect(app).to.be.an('object');
+  });
+
+});
