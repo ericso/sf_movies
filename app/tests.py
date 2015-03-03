9ab3cbc49422a86c4c7988d19031109030f5c09b
Eric So <me@eric.so>
2015-03-02 18:48:19 -0800

    Update tests

--- app/tests.py
+++ app/tests.py
@@ -6,15 +6,21 @@ from app.views import home
 
 class HomePageTest(TestCase):
 
-  def test_root_url_resolves_to_the_home_page(self):
+  def test_root_url_resolves_to_the_home_function(self):
 
     # Make sure we're calling the home view function
     found = resolve('/')
     self.assertEqual(found.func, home)
 
 
-  def test_home_page_renders_home_template(self):
+  def test_home_renders_home_template(self):
 
     # Make sure we're rendering the home template
     response = self.client.get('/')
     self.assertTemplateUsed(response, 'home.html')
+
+
+  def test_home_returns_google_api_key_context_var(self):
+
+    response = self.client.get('/')
+    self.assertIn('GOOGLE_API_KEY', response.context)
--- app/views.py
+++ app/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 def home(request):
   """Renders the home view of the app
   """
-  return render(request, 'home.html', {})
+  return render(request, 'home.html', {'GOOGLE_API_KEY': 'AIzaSyCq2vRo28y8UnbW2kwxn2oihcXBc0eH1iE'})
