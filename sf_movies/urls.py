a50bc9734f65461bfb376f4c8301d54bafcde21f
Eric So <me@eric.so>
2015-03-04 23:31:25 -0800

    Trying static url in urls.py

--- sf_movies/urls.py
+++ sf_movies/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+
 
 urlpatterns = patterns('',
 
@@ -7,4 +9,7 @@ urlpatterns = patterns('',
     url(r'^locations/', include('locations.urls')),
 
     # url(r'^admin/', include(admin.site.urls)),
+
+    # for static files
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
