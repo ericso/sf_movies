dbd9738d2985d9c4092007776bcf37a5921c5da3
Eric So <me@eric.so>
2015-03-04 23:15:13 -0800

    ...

--- sf_movies/settings.py
+++ sf_movies/settings.py
@@ -77,16 +77,11 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'static'
-# STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
-
 STATICFILES_DIRS = (
-  os.path.abspath(os.path.join(BASE_DIR, 'app', 'static')),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 
